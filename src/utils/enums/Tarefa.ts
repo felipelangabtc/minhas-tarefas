ac28f0c54480f7baa561ae976af910c892838620
Felipe <felipelangabtc@outlook.com>
2025-07-26 21:18:11 +1000

    Finalizando Parte 1 do Projeto

---
+++ src/utils/enums/Tarefa.ts
@@ -0,0 +1,10 @@
+export enum Prioridade {
+  URGENTE = 'urgente',
+  IMPORTANTE = 'importante',
+  NORMAIS = 'normais'
+}
+
+export enum Status {
+  PENDENTE = 'pendente',
+  CONCLUIDA = 'concluída'
+}
